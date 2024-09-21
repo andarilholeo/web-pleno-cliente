1db35aa643307e7875599088fa5639365036cc47
leonardo.santos <leonardosantos2742@gmail.com>
2024-09-21 17:33:25 -0300

    subindo repository, service e controller de Endereco

---
+++ WebPlenoCliente.Application/Services/EnderecoService/EnderecoService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using WebPlenoCliente.Application.DTO;
+using WebPlenoCliente.Domain.Entidades;
+using WebPlenoCliente.Domain.Interfaces;
+
+namespace WebPlenoCliente.Application.Services.EnderecoService
+{
+    public class EnderecoService : IEnderecoService
+    {
+        private readonly IEnderecoRepository _enderecoRepository;
+        private readonly IMapper _mapper;
+
+        public EnderecoService(IEnderecoRepository enderecoRepository, IMapper mapper)
+        {
+            _enderecoRepository = enderecoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<EnderecoDTO>> BuscarEnderecosPorClienteAsync(int clienteId)
+        {
+            var enderecos = await _enderecoRepository.BuscarEnderecosPorClienteAsync(clienteId);
+            return _mapper.Map<IEnumerable<EnderecoDTO>>(enderecos);
+        }
+
+        public async Task<EnderecoDTO> BuscarEnderecoPorIdAsync(int id)
+        {
+            var endereco = await _enderecoRepository.BuscarEnderecoPorIdAsync(id);
+            return _mapper.Map<EnderecoDTO>(endereco);
+        }
+
+        public Task<IEnumerable<EnderecoDTO>> BuscarEnderecosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<EnderecoDTO> AdicionarEnderecoAsync(EnderecoDTO enderecoDto)
+        {
+            var endereco = _mapper.Map<Endereco>(enderecoDto);
+            var novoEndereco = await _enderecoRepository.AdicionarEnderecoAsync(endereco);
+            return _mapper.Map<EnderecoDTO>(novoEndereco);
+        }
+
+        public async Task AlterarEnderecoAsync(EnderecoDTO enderecoDto)
+        {
+            var endereco = _mapper.Map<Endereco>(enderecoDto);
+            await _enderecoRepository.AlterarEnderecoAsync(endereco);
+        }
+
+        public async Task DeletarEnderecoAsync(int id)
+        {
+            await _enderecoRepository.DeletarEnderecoAsync(id);
+        }
+    }
+}
---
+++ WebPlenoCliente.Domain/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebPlenoCliente.Domain.Entidades;
+
+namespace WebPlenoCliente.Domain.Interfaces
+{
+    public interface IEnderecoRepository
+    {
+        Task<IEnumerable<Endereco>> BuscarEnderecosPorClienteAsync(int clienteId);
+        Task<Endereco> BuscarEnderecoPorIdAsync(int id);
+        Task<Endereco> AdicionarEnderecoAsync(Endereco endereco);
+        Task AlterarEnderecoAsync(Endereco endereco);
+        Task DeletarEnderecoAsync(int id);
+    }
+}
